63607288b3ef3d0d1b8ec0a2acaa2e99d8643144
TianQingChe <tianqingcheyyf@gmail.com>
2018-06-24 22:28:04 +0800

    static
    新增html静态模板文件

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
+const { normal } = require('./template')
 app.use(async (ctx, next) => {
-	ctx.body = 'Home Page'
+	ctx.type = 'text/html; charset=utf-8'
+	ctx.body = normal
 })
 app.listen(4455)
